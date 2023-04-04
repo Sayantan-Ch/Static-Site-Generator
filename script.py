47caac52f503675bffc880277bf9591b56d33bd5
Sayantan Chatterjee <sayantan1298@gmail.com>
2023-04-05 01:39:30 +0530

    Testing Basic Templating

---
+++ script.py
@@ -0,0 +1,8 @@
+import jinja2
+title = 'Hi this is my title'
+outputfile = 'myfile.html'
+subs = jinja2.Environment( 
+              loader=jinja2.FileSystemLoader('./')      
+              ).get_template('template.html').render(title=title) 
+# lets write the substitution to a file
+with open(outputfile,'w') as f: f.write(subs)
